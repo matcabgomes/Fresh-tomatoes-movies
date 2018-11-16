c26f71b2b881b0c41648be7bc8405f159c6f1e7f
Matheus Gomes Silva <matcabgomes@gmail.com>
2018-11-16 00:40:16 -0200

    Alterações de código, para a utilização do PEP8

--- media.py
+++ media.py
@@ -1,15 +1,16 @@
 import webbrowser
 
-class Movie(): 
+
+class Movie():
     """A class that representes a movie"""
 
-    #initialize the class with the info passed in those params
-    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+#    initialize the class with the info passed in those params
+    def __init__(self, title, storyline, poster_img_url, trailer_yt_url):
         self.title = title
         self.storyline = storyline
-        self.poster_image_url = poster_image_url
-        self.trailer_youtube_url = trailer_youtube_url
+        self.poster_image_url = poster_img_url
+        self.trailer_youtube_url = trailer_yt_url
 
-    #show the movie trailer passad in the trailer_youtube_url param
+#   show the movie trailer passad in the trailer_youtube_url param
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
